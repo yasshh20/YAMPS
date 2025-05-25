40dbec820fc32a06d03717f0762e8878d972b245
Yash <yash@codewithyash.com>
2025-05-26 01:53:00 +0530

    updated imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Success from "./pages/Success";
 import Error from "./pages/Error";
 import toast, { Toaster } from "react-hot-toast";
-import { ProtectedRoute } from "./component/protectedRoute";
-// ✅ Fixed: removed extra space
+import { ProtectedRoute } from "./component/pro"; // ✅ Fixed: correct casing, no extra space
 
 const App = () => {
   return (
